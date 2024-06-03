a16be8903cd2d836fe2e5c385c28b895a7eee9db
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-06-03 11:24:59 +0300

    feat(auth):add mailing initial structure

--- src/app/shared/mailer/mailer.service.ts
+++ src/app/shared/mailer/mailer.service.ts
@@ -1,13 +1,20 @@
-import { PatientService } from '@modules/patients/patient/patient.service';
+import { CompanionRepository } from '@modules/companions/companion/repositories';
+import { Companion } from '@modules/companions/companion/schemas';
+import { PatientRepository } from '@modules/patients/patient/repositories';
+import { Patient } from '@modules/patients/patient/schema/patient.schema';
 import { Injectable } from '@nestjs/common';
+import { UserType } from '@shared/enums';
 import * as nodemailer from 'nodemailer';
 import { mailTypes } from './enums';
 
 @Injectable()
 export class MailerService {
-	constructor(private readonly patientService: PatientService) {}
+	constructor(
+		private readonly patientRepository: PatientRepository,
+		private readonly companionRepository: CompanionRepository,
+	) {}
 
-	mailTransporter() {
+	mailTransporter(): nodemailer.Transporter {
 		const Transporter = nodemailer.createTransport({
 			service: 'gmail',
 			host: 'smtp.gmail.com',
@@ -42,23 +49,39 @@ export class MailerService {
 	};
 
 	// FIXME replace OTP with interface that represent data and based on it check what we need to replace
-	async sendEmail(patientId: string, Otp: string, mailType: mailTypes): Promise<void> {
-		const [patient] = await this.patientService.findPatients({ id: patientId });
+	async sendEmail(
+		userId: string,
+		Otp: string,
+		mailType: mailTypes,
+		userType: UserType,
+	): Promise<void> {
+		let user: Patient | Companion;
+		switch (userType) {
+			case UserType.PATIENT:
+				user = await this.patientRepository.findById(userId);
+				break;
+			case UserType.COMPANION:
+				user = await this.companionRepository.findById(userId);
+				break;
+			default:
+				throw new Error('User type not found');
+		}
+
 		const format = this.mailFormats[mailType];
 
 		const textWithOTP = format.text.replace('{{OTP}}', Otp);
 
 		const mailOptions = {
 			from: process.env.MAILER_EMAIL,
-			to: patient.email,
+			to: user.email,
 			subject: format.subject,
 			text: textWithOTP,
 		};
 
 		const transporter = this.mailTransporter();
-		transporter.sendMail(mailOptions, function (error, info) {
+		transporter.sendMail(mailOptions, function (error: unknown, info: unknown) {
 			if (error) {
-				throw new Error('Error sending email');
+				throw new Error(`Error sending email ${info}`);
 			}
 		});
 	}
