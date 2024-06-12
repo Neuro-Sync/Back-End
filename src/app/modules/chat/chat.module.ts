0b65f65b25292733dfae71f719e4669b56872a67
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-06-12 13:22:19 +0300

    feat(chat):add chat initial socket testing

--- src/app/modules/chat/chat.module.ts
+++ src/app/modules/chat/chat.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatController } from './chat.controller';
+import { ChatGateway } from './chat.getway';
 import { ChatService } from './chat.service';
 import { MessageRepository } from './repositories/message.repository';
 import { Message, MessageSchema } from './schemas';
@@ -8,6 +9,6 @@ import { Message, MessageSchema } from './schemas';
 @Module({
 	imports: [MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }])],
 	controllers: [ChatController],
-	providers: [ChatService, MessageRepository],
+	providers: [ChatService, MessageRepository, ChatGateway],
 })
 export class ChatModule {}
